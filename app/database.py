203a8d05616bd5a759956d3e326bb955a7920a5a
Bordovichek <slava.khomiakov2019@gmail.com>
2026-01-13 18:41:54 +0300

    Update files and remove unnecessary main.py

--- app/database.py
+++ app/database.py
@@ -3,14 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# URL берем из переменных окружения для гибкости Docker
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/deribit_db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Dependency для FastAPI
 def get_db():
     db = SessionLocal()
     try:
